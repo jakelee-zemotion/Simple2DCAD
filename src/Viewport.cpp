ad1d77845277af0f26ac7d12797224c62f3bb38e
Jake <jake.lee@z-emotion.com>
2023-11-30 10:37:02 +0900

    Override paintEvent()

--- src/Viewport.cpp
+++ src/Viewport.cpp
@@ -1,7 +1,25 @@
 #include "Viewport.h"
+#include <QPainter>
 
 Viewport::Viewport(QWidget* parent)
 	:QWidget(parent)
 {
 
+}
+
+void Viewport::paintEvent(QPaintEvent* event)
+{
+    QPainter MyPainter(this);
+    MyPainter.setPen(Qt::blue);
+    MyPainter.setFont(QFont("Arial", 80));
+    MyPainter.drawText(rect(), Qt::AlignCenter, "Qt");
+
+    QPen LinePen(Qt::green);
+    LinePen.setWidth(2);
+
+    QPoint p1(0, 0);
+    QPoint p2(this->size().width(), this->size().height());
+
+    MyPainter.setPen(LinePen);
+    MyPainter.drawLine(p1, p2);
 }
--- src/Viewport.h
+++ src/Viewport.h
@@ -5,5 +5,8 @@ class Viewport : public QWidget
 {
 public:
 	Viewport(QWidget* parent = 0);
+
+protected:
+	void paintEvent(QPaintEvent* event) override;
 };
 
